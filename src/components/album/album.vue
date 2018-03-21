55b9a86a107ab2026bea40c604026679d93dd997
SmallBoys <554645932@qq.com>
2018-03-21 11:29:41 +0800

    开始编写歌单页面

---
+++ src/components/album/album.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    ss
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" type="text/css">
+</style>
